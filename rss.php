b3057ccded43b4dc562cd75cc1ad95c2c7185854
Christophe HENRY <christophe.henry@sbgodin.fr>
2013-12-04 20:19:18 +0100

    Updates all the emails of Timo
    find . -type f -print0 | xargs -0 sed -i 's/<ti-mo@myopera.com>/<timo@neerden.eu>/g'

--- rss.php
+++ rss.php
@@ -4,7 +4,7 @@
 # http://lehollandaisvolant.net/blogotext/
 #
 # 2006      Frederic Nassar.
-# 2010-2013 Timo Van Neerden <ti-mo@myopera.com>
+# 2010-2013 Timo Van Neerden <timo@neerden.eu>
 #
 # BlogoText is free software.
 # You can redistribute it under the terms of the MIT / X11 Licence.
