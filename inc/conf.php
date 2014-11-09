a1b092c56b0b8bb51c199122c9cb03d55adcba16
timovn <levoltigeurhollandais@gmail.com>
2014-11-09 16:08:16 +0100

    (version)

--- inc/conf.php
+++ inc/conf.php
@@ -19,7 +19,7 @@ if (!empty($GLOBALS['fuseau_horaire'])) {
 }
 
 // BLOGOTEXT VERSION (do not change it)
-$GLOBALS['version'] = '2.1.0.0';
+$GLOBALS['version'] = '2.1.0.1';
 $GLOBALS['last-online-file'] = '../config/version.txt';
 
 // MINIMAL REQUIRED PHP VERSION
