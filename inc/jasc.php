d7a2dd84991581ad8877f83f6de0fe67ffe15e8a
timovn <fire@lehollandaisvolant.net>
2015-01-28 18:56:00 +0100

    [JS] “leaving page w/o saving changes”-popup bugfix for Opera-Webkit
    Little hack that now works in Opera, Webkit and Firefox ; as before the popup showed up everytime, even when you submited the form, in Opera Webkit.

--- inc/jasc.php
+++ inc/jasc.php
@@ -228,9 +228,9 @@ var contenuLoad = document.getElementById("contenu").value;
 window.addEventListener("beforeunload", function (e) {
 	// From https://developer.mozilla.org/en-US/docs/Web/Reference/Events/beforeunload
 	var confirmationMessage = \''.$GLOBALS['lang']['question_quit_page'].'\';
-	if(document.getElementById("contenu").value == contenuLoad) { confirmationMessage = null };
-	(e || window.event).returnValue = confirmationMessage || \'\' ;	//Gecko + IE ; Gecko show popup if "null" but not if empty str,
-	return confirmationMessage;													// Webkit.
+	if(document.getElementById("contenu").value == contenuLoad) { return true; };
+	(e || window.event).returnValue = confirmationMessage || \'\' ;	//Gecko + IE
+	return confirmationMessage;													// Webkit : ignore this.
 });
 ';
 	if ($a == 1) {
