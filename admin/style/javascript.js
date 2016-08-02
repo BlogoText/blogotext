f35b873d4e7db7133c911292cdc7ed3bddf161c5
timovn <fire@lehollandaisvolant.net>
2016-08-02 18:59:53 +0200

    [PHP] - Fix for crashing on previsualising a comment
    also forgot to remove some JS in admin
    
    Added JS in public theme to scroll to form if some form errors occured (like captcha errors).

--- admin/style/javascript.js
+++ admin/style/javascript.js
@@ -1180,7 +1180,6 @@ function refresh_all_feeds(refreshLink) {
 	}
 	xhr.onload = function() {
 		var resp = this.responseText;
-		alert(resp);
 
 		// update status
 		var nbNewFeeds = resp.substr(resp.indexOf("Success")+7);
