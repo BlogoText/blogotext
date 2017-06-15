5ce1bffb9bf6a0453f1fcf7ca0cc79a25c4dc303
Gaspard Witrand <gaspard.witrand@epitech.eu>
2017-06-15 23:06:53 +0200

    Patch preference error - #293

--- admin/preferences.php
+++ admin/preferences.php
@@ -155,9 +155,8 @@ function list_themes($path)
 {
     if ($handler = opendir($path)) {
         while ($folders = readdir($handler)) {
-            if (is_file($path.'/'.$folders.'/list.html')) {
+            if (is_dir($path.'/'.$folders) && is_file($path.'/'.$folders.'/list.html'))
                 $themes[$folders] = $folders;
-            }
         }
         closedir($handler);
     }
